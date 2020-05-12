0ec341d9a8bf1619cafcdf57600f01c1403d700f
Simon <s.graham.1@warwick.ac.uk>
2020-05-12 14:47:29 +0100

    optimisation

--- hover/misc/viz_utils.py
+++ hover/misc/viz_utils.py
@@ -4,62 +4,84 @@ import math
 import colorsys
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
 
 from .utils import bounding_box
 
-####
 
-def class_colour(value):
-    """
-    Generate colours per class.
-    To get visually distinct colors, generate them in HSV space then
-    convert to RGB.
-    """
 
-    if value == 1:
-        return (255,0,0)
-    elif value == 2:
-        return (0,255,0)
-    elif value == 3:
-        return (0,0,255)
-    elif value == 4:
-        return (255, 255, 0)
-    elif value == 5:
-        return (255, 165, 0)
-####
-
-def visualize_instances(mask, type_mask, canvas=None):
+def class_colour(class_value):
     """
+    Generate RGB colour for overlay based on class id
     Args:
-        mask: array of NW
-    Return:
-        Image with the instance overlaid
+        class_value: integer denoting the class of object  
     """
+    if class_value == 0:
+        return 0, 0, 0  # black (background)
+    if class_value == 1:
+        return 255, 0, 0  # red
+    elif class_value == 2:
+        return 0, 255, 0  # green
+    elif class_value == 3:
+        return 0, 0, 255  # blue
+    elif class_value == 4:
+        return 255, 255, 0  # yellow
+    elif class_value == 5:
+        return 255, 165, 0  # orange
+    elif class_value == 6:
+        return 0, 255, 255  # cyan
+    else:
+        raise Exception(
+            'Currently, overlay_segmentation_results() only supports up to 6 classes.')
+####
 
-    canvas = np.full(mask.shape + (3,), 200, dtype=np.uint8) \
-                if canvas is None else np.copy(canvas)
+def visualize_instances(input_image, predict_instance, predict_type=None, line_thickness=2):
+    """
+    Overlays segmentation results on image as contours
+    Args:
+        input_image: input image
+        predict_instance: instance mask with unique value for every object
+        predict_type: type mask with unique value for every class
+        line_thickness: line thickness of contours
+    Returns:
+        overlay: output image with segmentation overlay as contours
+    """
+   
+    overlay = np.copy((input_image).astype(np.uint8))
 
-    insts_list = list(np.unique(mask))
-    insts_list.remove(0) # remove background
+    if predict_type is not None:
+        type_list = list(np.unique(predict_type))  # get list of types
+        type_list.remove(0)  # remove background
+    else:
+        type_list = [4]  # yellow
 
-    for idx, inst_id in enumerate(insts_list):
-        inst_map = np.array(mask == inst_id, np.uint8)
-        if type_mask is not None:
-            inst_type = inst_map * type_mask
-            inst_type = np.max(np.unique(inst_type))
+    for iter_type in type_list:
+        if predict_type is not None:
+            label_map = (predict_type == iter_type) * predict_instance
         else:
-            inst_type = 4 # yellow
-        y1, y2, x1, x2  = bounding_box(inst_map)
-        y1 = y1 - 2 if y1 - 2 >= 0 else y1 
-        x1 = x1 - 2 if x1 - 2 >= 0 else x1 
-        x2 = x2 + 2 if x2 + 2 <= mask.shape[1] - 1 else x2 
-        y2 = y2 + 2 if y2 + 2 <= mask.shape[0] - 1 else y2 
-        inst_map_crop = inst_map[y1:y2, x1:x2]
-        inst_canvas_crop = canvas[y1:y2, x1:x2]
-        contours = cv2.findContours(inst_map_crop, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(inst_canvas_crop, contours[0], -1, class_colour(inst_type), 2)
-        canvas[y1:y2, x1:x2] = inst_canvas_crop        
-    return canvas
+            label_map = predict_instance
+        instances_list = list(np.unique(label_map))  # get list of instances
+        instances_list.remove(0)  # remove background
+        contours = []
+        for inst_id in instances_list:
+            instance_map = np.array(
+                predict_instance == inst_id, np.uint8)  # get single object
+            y1, y2, x1, x2 = bounding_box(instance_map)
+            y1 = y1 - 2 if y1 - 2 >= 0 else y1
+            x1 = x1 - 2 if x1 - 2 >= 0 else x1
+            x2 = x2 + 2 if x2 + 2 <= predict_instance.shape[1] - 1 else x2
+            y2 = y2 + 2 if y2 + 2 <= predict_instance.shape[0] - 1 else y2
+            inst_map_crop = instance_map[y1:y2, x1:x2]
+            contours_crop = cv2.findContours(
+                inst_map_crop, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            index_correction = np.asarray([[[[x1, y1]]]])
+            for i in range(len(contours_crop[0])):
+                contours.append(
+                    list(np.asarray(contours_crop[0][i].astype('int32')) + index_correction))
+        contours = list(itertools.chain(*contours))
+        cv2.drawContours(overlay, np.asarray(contours), -1,
+                         class_colour(iter_type), line_thickness)
+    return overlay
 ####
 
 def gen_figure(imgs_list, titles, fig_inch, shape=None,
